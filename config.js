14d3f3f02f46e81b56391a49d6196731e3f20ea9
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-08-14 06:41:36 +0000

    chore: add rimraf, sessionPath, and ignore sessions
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
+const path = require('node:path');
+
 module.exports = {
   'qr': {
-    'store': 'web', // store type, you can choose 'web', 'terminal', or 'file'
+    'store': 'file', // store type, you can choose 'web', 'terminal', or 'file'
     'options': {
       // store options (required for 'web', and 'file')
-      'port': 8080, // port (required for 'web'),
-      // 'dest': 'qr.png', // destination (required for 'file')
+      // 'port': 8080, // port (required for 'web'),
+      'dest': path.resolve('qr.png'), // destination (required for 'file')
     },
   },
   'prefixes': ['.'], // Prefixes for the commands
+  'sessionPath': './sessions', // Path to the sessions folder
 };
