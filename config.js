ff2afc35d755643a7ecdc515e06e0a8e0957ead8
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-09-23 07:36:06 +0000

    chore: set qr store to web
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const { SessionManager } = require('gampang');
-const path = require('node:path');
+// const path = require('node:path');
 
 module.exports = {
   'qr': {
-    'store': 'file', // store type, you can choose 'web', 'terminal', or 'file'
+    'store': 'web', // store type, you can choose 'web', 'terminal', or 'file'
     'options': {
       // store options (required for 'web', and 'file')
-      // 'port': 8080, // port (required for 'web'),
-      'dest': path.resolve('qr.png'), // destination (required for 'file')
+      'port': 8080, // port (required for 'web'),
+      // 'dest': path.resolve('qr.png'), // destination (required for 'file')
     },
   },
   'prefixes': ['.'], // Prefixes for the commands
